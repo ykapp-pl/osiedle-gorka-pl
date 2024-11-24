2ff088d266d31b938fca2a3e7ba44cc46b65dad2
Yakubus <yaqbg1@gmail.com>
2024-11-24 23:44:11 +0100

    tmp nasa api adding

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,70 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="wrapper">
+    <div class="search">
+        <label for="searchInput" class="search">Serch</label>
+        <input
+          id="search"
+          name="search"
+          v-model= "searchValue"
+          @input="handleInput"
+        />
+      <ul>
+        <li v-for="item in results" :key="item.data[0].nasa_id">
+          <p>{{item.data[0].description}}</p>
+        </li>
+
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from 'axios'
+import debounce from 'lodash.debounce'
+
+const API = 'https://images-api.nasa.gov/'
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      searchValue: '',
+      results: []
+    }
+  },
+  methods: {
+    handleInput: debounce(function () {
+      axios.get(`${API}search?q=${this.searchValue}&media_type=image`)
+        .then((response) => {
+          this.results = response.data.collection.items
+        }).catch((error) => {
+          console.log(error)
+        })
+    }, 500)
   }
 }
 </script>
+
+<style lang="scss" scoped >
+.wrapper{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 0 auto;
+  padding: 30px;
+  width: 100%;
+}
+.search{
+  display: flex;
+  flex-direction: column;
+  width: 300px;
+  input{
+    height: 30px;
+    border: 0;
+    border-bottom: 1px solid #ccc;
+  }
+  p{
+    font-family: Montserrat, sans-serif;
+  }
+}
+</style>
